44d871fe18df55f62620c4b8319c3bed12d1c868
Filipterra <37516675+Filipterra@users.noreply.github.com>
2019-04-15 22:13:56 +0200

    Mock JUnit test

---
+++ src/test/java/com/shopcrud/alpha/JunitTests.java
@@ -0,0 +1,11 @@
+package com.shopcrud.alpha;
+
+import org.junit.jupiter.api.Test;
+ 
+class JunitTests { //We should see this class while executing "mvn test"
+ 
+    @Test
+    void justAnExample() {
+ 
+    }
+}
